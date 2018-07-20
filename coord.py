7974aaf2471694b32d8787c0f1e3ce7ba8274eb0
Matthew James <matthew.stephen.james@gmail.com>
2018-07-21 03:33:46 +0800

    near coords

--- coord.py
+++ coord.py
@@ -27,6 +27,18 @@ class Coord:
         ]
         adjs = [self.__add__(d) for d in diffs]
         return [a for a in adjs if a.in_matrix(R)]
+    
+    def near(self, R):
+        diffs = []
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                for z in range(-1, 2):
+                    if x==0 and y==0 and z==0:
+                        pass
+                    elif abs(x)==1 and abs(y)==1 and abs(z)==1:
+                        pass
+                    else:
+                        diffs.push(x, y, z)
 
 # note: don't construct Diff objects directly; use diff() func to get correct subclass
 def diff(dx, dy, dz):
