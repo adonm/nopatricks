37a70ed8718b2314a5ff5490740245b41ae6b094
Scott Percival <scott.percival@dbca.wa.gov.au>
2018-07-21 00:55:17 +0800

    more props

--- commands.py
+++ commands.py
@@ -23,6 +23,35 @@ class LMove( mrc.Block ):
     sld2i = mrc.Bits( 0x00, 0b0000000011110000, size=2 )
     sld1i = mrc.Bits( 0x00, 0b0000000000001111, size=2 )
 
+    @property
+    def sld2x( self ):
+        return 0 if self.sld2a != 0b01 else (self.sdl2i + 5)
+
+    @property
+    def sld2y( self ):
+        return 0 if self.sld2a != 0b10 else (self.sdl2i + 5)
+
+    @property
+    def sld2z( self ):
+        return 0 if self.sld2a != 0b11 else (self.sdl2i + 5)
+
+    @property
+    def sld1x( self ):
+        return 0 if self.sld1a != 0b01 else (self.sdl1i + 5)
+
+    @property
+    def sld1y( self ):
+        return 0 if self.sld1a != 0b10 else (self.sdl1i + 5)
+
+    @property
+    def sld1z( self ):
+        return 0 if self.sld1a != 0b11 else (self.sdl1i + 5)
+
+    @property
+    def repr( self ):
+        return 'sld1: ({}, {}, {}), sld2: ({}, {}, {})'.format( self.sld1x, self.sld1y, self.sld1z, self.sld2x, self.sld2y, self.sld2z )
+
+
 class FusionP( mrc.Block ):
     const = mrc.Const( mrc.Bits( 0x00, 0b00000111 ), 0b111 )
     nd = mrc.Bits( 0x00, 0b11111000 )
@@ -39,6 +68,8 @@ class Fission( mrc.Block ):
 class Fill( mrc.Block ):
     const = mrc.Const( mrc.Bits( 0x00, 0b00000111, size=1 ), 0b011 )
     nd = mrc.Bits( 0x00, 0b11111000, size=1 )
+
+
 
 
 a = LMove()
