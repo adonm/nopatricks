ec11985a64fe0ebe497406d9a10e3a79f009185b
Adon Metcalfe <adonm@adonm-inspiron.localdomain>
2018-07-21 02:43:51 +0800

    rename adjacent

--- coord_test.py
+++ coord_test.py
@@ -4,12 +4,12 @@ class TestCoords(unittest.TestCase):
 
     def test_adj_6(self):
         c = Coord(1,1,1)
-        acs = adjacent_coords(c, 10)
+        acs = c.adjacent(10)
         self.assertEqual(len(acs), 6)
 
     def test_adj_5(self):
         c = Coord(0,1,1)
-        acs = adjacent_coords(c, 10)
+        acs = c.adjacent(10)
         self.assertEqual(len(acs), 5)
 
 if __name__ == '__main__':
