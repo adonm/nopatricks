a64ae8a709e6ff879e1b9951bb8ba012540abe5e
sqweek <sqweek@gmail.com>
2018-07-21 13:28:51 +0800

    scan: experimental partitioner API

--- scan.py
+++ scan.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from abc import ABC, abstractmethod
+from math import ceil
+
+# Note Area doesn't define the type of coordinates; it happily works with Coord or tuples
 class Area:
     def __init__(self, id, coord):
         self.id = id # 1-based
@@ -34,3 +38,39 @@ def flood_fill(plane, areas, area, start):
                 area.grow(n)
                 stack.append(n)
 
+
+
+
+# Partitioner is supposed to break large Areas up into smaller Areas suitable for one bot
+# Might be useful later but I realised it's probably simpler to prevent scan/fill from creating
+# large areas in the first place.
+class Partitioner(ABC):
+    @abstractmethod
+    def partition_area(plane, area, subdivisions):
+        pass
+
+
+class PartitionSquares(Partitioner):
+    def __init__(self, R):
+        # arbitrary hueristic
+        self.target_pts_per_bot = (R * R) / 20.0 / 2.0
+
+    def partition(plane, areas):
+        jobs = []
+        total_pts = sum([len(a.points) for a in areas])
+        split = min(int(total_pts / self.target_pts_per_bot), 20)
+        pts_per_job = total_pts / split
+        for area in areas:
+            if len(area.points) / float(pts_per_job) < 1.25:
+                # small area; queue it up and disregard
+                jobs.add(area)
+                split -= 1
+                total_pts -= len(area.points)
+                pts_per_job = total_pts / split
+
+        for area in areas:
+            if area not in jobs:
+                sub_areas = partition_area(plane, area, ceil(len(area.points) / pts_per_job))
+                jobs.extend(sub_areas)
+
+        return jobs
