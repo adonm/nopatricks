65fad4f2fcf63c2caabadda356721f3d77a0b707
Matthew James <matthew.stephen.james@gmail.com>
2018-07-21 16:27:51 +0800

    working submit

--- runall.sh
+++ runall.sh
@@ -2,5 +2,6 @@
 
 for P in {2..186}
 do
+    echo "Problem $P"
 	python3.6 algorithm.py $P
 done
--- submit.sh
+++ submit.sh
@@ -4,11 +4,11 @@ do
 done
 
 cd submission
-zip ../submission.zip *
+zip ../submission.zip ./LA*.nbt
 cd ..
 
 SHA=`openssl sha -sha256 submission.zip | cut -d ' ' -f 2`
-URL=`curl --upload-file ./submission.zip https://transfer.sh/submission.zip`
+URL=https://www.dropbox.com/s/zii6lvk3qckp81g/submission.zip?dl=0
 
 curl -L \
   --data-urlencode action=submit \
@@ -17,3 +17,12 @@ curl -L \
   --data-urlencode submissionSHA=$SHA \
   https://script.google.com/macros/s/AKfycbzQ7Etsj7NXCN5thGthCvApancl5vni5SFsb1UoKgZQwTzXlrH7/exec
 
+rm -rf test
+mkdir test
+cd test
+curl -L -s -S -f -o submission.zip $URL
+echo $SHA
+echo `openssl sha -sha256 submission.zip | cut -d ' ' -f 2`
+unzip submission.zip
+cd ..
+rm -rf test
