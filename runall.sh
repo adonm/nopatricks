414a836dcc12d914db19ab92adf8121855bc17ac
Matthew James <matthew.stephen.james@gmail.com>
2018-07-23 22:34:17 +0800

    FR problems

--- runall.sh
+++ runall.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-for P in {1..186}
+for P in {19..186}
 do
     echo "Problem $P"
 	python3.6 algorithm_cube.py $P
+done
+
+for P in {19..186}
+do
+    echo "Problem $P"
+	python3.6 unprinter.py $P
+done
+
+for P in $(seq -f "%03g" 2 115)
+do
+    echo "Problem $P"
+	python3.6 launcher.py --source problemsF/FR$P"_src.mdl" --target problemsF/FR$P"_tgt.mdl" submission/FR$P".nbt"
 done
